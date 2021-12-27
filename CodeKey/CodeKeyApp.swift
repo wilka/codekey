062c0e683c1c90ae4e9bb6f67f9748baa815076e
Wilka Hudson <wilka.hudson@gmail.com>
2021-12-27 11:35:24 +0000

    Initial Commit

---
+++ CodeKey/CodeKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeKeyApp.swift
+//  CodeKey
+//
+//  Created by Wilka Hudson on 27/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct CodeKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeKey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeKey
+//
+//  Created by Wilka Hudson on 27/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
